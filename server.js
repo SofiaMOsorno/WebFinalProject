d8535cd958374ed4b3822666f24891c9aabc972a
CrossBeast78 <167384823+CrossBeast78@users.noreply.github.com>
2024-11-25 17:00:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const router = require('./app/Controllers/router'); 
 const PORT = 3000;
 const app = express();
+const cors = require('cors');
+const connectToDatabase = require('./app/Data/db'); // Importar conexión a MongoDB
+const productRoutes = require('./app/Controllers/productRoutes'); 
+const orderRoutes = require('./app/Controllers/orderRoutes'); // Ruta para órdenes
+const meseroRoutes = require('./app/Controllers/meseroRoutes');
 
 app.use(express.json());
 app.use(router);
-
+app.use(cors());
 app.use(express.static('app'));
 app.use('/Views', express.static('Views'));
+
+
+connectToDatabase();
+
+app.use('/api/products', productRoutes);
+app.use('/api/user', meseroRoutes);
+app.use('/api/orders', orderRoutes); // Usar la ruta
 
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`);

2ce27ce4d33f95b30cdc418af655d4129213605d
CrossBeast78 <167384823+CrossBeast78@users.noreply.github.com>
2024-11-25 16:58:10 -0600

    Add files via upload

---
+++ app/Data/meseroModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+// Definición del esquema y modelo de usuario
+const userSchema = mongoose.Schema({
+    nummesero: {
+        type: Number,
+        required: true
+    },
+    nombre: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    edad: {
+        type: Number,
+        min: 0,
+        max: 100,
+        required: true
+    },
+    puesto: {
+        type: String,
+        required: true
+    },
+    perfil: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+module.exports = User;
